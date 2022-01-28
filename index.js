4a790a4e08633d4c830d7b8f0c9f900f1e6f8892
Shaun Porter <shaunoman840@gmail.com>
2022-01-28 14:59:33 +0000

    test

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'canvas',
-    loop: true,
-    autoplay: true,
-    path: "intro.json"
-  })
+var anim = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'canvas',
+  loop: true,
+  autoplay: true,
+  path: "intro.json",
+  rendererSettings: {
+    preserveAspectRatio: 'none'
+  }
+});
+
+// anim.addEventListener("data_ready", complete);
+
+// //set the attribute manually after the svg is loaded.
+// function complete() {
+//   $('#sec svg').attr('preserveAspectRatio', 'xMidYMax slice');
+// }
